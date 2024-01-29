1e8a1b9d96cbec20f44bbb465f4a6b4a180240a6
AnettX <143034831+AnettX@users.noreply.github.com>
2024-01-29 19:13:10 +0000

    Deploying to gh-pages from @ AnettX/goit-js-hw-10@28ad19bf16ebaa28ae5d5a0d2d51187863447fba 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const o=document.querySelector('input[type="number"]');console.log(o);const i=document.querySelectorAll('input[type="radio"]');console.log(i);const s=document.querySelectorAll('button[type="submit"]');console.log(s);o.addEventListener("input",r);function r(e){return new Promise((t,n)=>{setTimeout(()=>{Math.random()>.5?t(`✅ Fulfilled promise in ${e} ms`):n(`❌ Rejected promise in ${e} ms`)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
